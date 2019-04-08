8c37b339010b0961615d38bfc663e968a6dc8c7c
Michael Williams <mjwilli8@umd.edu>
2019-04-07 20:43:11 -0400

    prototyped simple networking for host and client. still needs work

---
+++ Island Survival/Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Mirror;
+
+[System.Serializable]
+public class ToggleEvent : UnityEvent<bool>{}
+
+public class Player : NetworkBehaviour
+{
+	[SerializeField] ToggleEvent onToggleShared;
+	[SerializeField] ToggleEvent onToggleLocal;
+	[SerializeField] ToggleEvent onToggleRemote;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnablePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DisablePlayer()
+    {
+    	onToggleShared.Invoke(false);
+
+    	if (isLocalPlayer)
+    	{
+    		onToggleLocal.Invoke(false);
+    	}
+
+    	else
+    	{
+    		onToggleRemote.Invoke(false);
+    	}
+    }
+
+    void EnablePlayer()
+    {
+    	onToggleShared.Invoke(true);
+    	
+    	if (isLocalPlayer)
+    	{
+    		onToggleLocal.Invoke(true);
+    	}
+
+    	else
+    	{
+    		onToggleRemote.Invoke(true);
+    	}
+    }
+}
