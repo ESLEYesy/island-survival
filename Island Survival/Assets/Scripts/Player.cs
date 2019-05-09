93ac4d1bf8c4967722b601cc594a8f7aed58cd4c
Michael Williams <mjwilli8@umd.edu>
2019-05-09 17:49:55 -0400

    added day/night cycle

--- Island Survival/Assets/Scripts/Player.cs
+++ Island Survival/Assets/Scripts/Player.cs
@@ -20,9 +20,9 @@ public class Player : NetworkBehaviour
         EnablePlayer();
 				if (isLocalPlayer)
 				{
-						Camera.main.transform.position = this.transform.position - this.transform.forward*5 + this.transform.up*5;
-						Camera.main.transform.LookAt(this.transform.position);
-						Camera.main.transform.parent = this.transform;
+					Camera.main.transform.position = this.transform.position - this.transform.forward*8 + this.transform.up*3;
+					Camera.main.transform.LookAt(this.transform.position);
+					Camera.main.transform.parent = this.transform;
 				}
 
 				//GetComponent<NetworkAnimator>().SetParameterAutoSend(0,true);
