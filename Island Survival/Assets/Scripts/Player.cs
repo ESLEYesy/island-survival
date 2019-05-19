ba882502fe3cccc29a81d5234b1dafaa55f29400
Michael Williams <mjwilli8@umd.edu>
2019-05-19 09:13:27 -0400

    fixed camera issue

--- Island Survival/Assets/Scripts/Player.cs
+++ Island Survival/Assets/Scripts/Player.cs
@@ -75,197 +75,199 @@ public class Player : NetworkBehaviour
             energy = 100;
 
             animator = gameObject.GetComponent<Animator>();
-		}
 
-        //GetComponent<NetworkAnimator>().SetParameterAutoSend(0,true);
+            //GetComponent<NetworkAnimator>().SetParameterAutoSend(0,true);
 
-        interactLabel = Instantiate(textMeshPrefab);
-        interaction = interactionRadius.GetComponent<PlayerInteraction>();
+            interactLabel = Instantiate(textMeshPrefab);
+            interaction = interactionRadius.GetComponent<PlayerInteraction>();
     
-        // Update health and energy every second
-        InvokeRepeating("UpdateEnergy", 1f, 1f);
-        InvokeRepeating("UpdateHealth", 1f, 1f);
+            // Update health and energy every second
+            InvokeRepeating("UpdateEnergy", 1f, 1f);
+            InvokeRepeating("UpdateHealth", 1f, 1f);
 
-        // Set current inventory item image
-        currInventoryIndex = 0;
-
+            // Set current inventory item image
+            currInventoryIndex = 0;
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CAMERA FOLLOW
-        Camera.main.transform.position = this.transform.position + camDiff;
-
-        //spawn item - K (axe)
-        if (Input.GetKeyDown("k"))
+        if (isLocalPlayer)
         {
-            GameObject spawnContainer = Instantiate(itemContainerPrefab, (this.transform.position + this.transform.forward * 1.01f + new Vector3(0f, 0.5f, 0f)), Random.rotation);
-            Axe newItem = spawnContainer.AddComponent(typeof(Axe)) as Axe;
-            Debug.Log("Spawned a " + newItem.Name + " " + newItem.Title + ".");
+            //CAMERA FOLLOW
+            Camera.main.transform.position = this.transform.position + camDiff;
 
-            spawnContainer.GetComponent<Rigidbody>().AddForce(this.transform.forward * throwForce);
-            spawnContainer.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f)));
-        }
-
-        if (Input.GetKeyDown("m"))
-        {
-            Debug.Log(Screen.currentResolution);
-            Debug.Log(Screen.height);
-            Debug.Log(Screen.width);
-        }
-
-        //drop item - X
-        if (equipped != null && Input.GetKeyDown("x"))
-        {
-            GameObject spawnContainer = Instantiate(itemContainerPrefab, (this.transform.position + this.transform.forward * 1.01f + new Vector3(0f, 0.5f, 0f)), Random.rotation);
-            Item newItem = spawnContainer.AddComponent(equipped.GetType()) as Item;
-            Debug.Log("Dropped a " + equipped.Name + ".");
-            inventoryPic001.gameObject.SetActive(false);
-            equipped = null;
-
-            spawnContainer.GetComponent<Rigidbody>().AddForce(this.transform.forward * throwForce);
-            spawnContainer.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f)));
-        }
-
-        //use item - MOUSE1
-        if (equipped != null && Input.GetMouseButtonDown(0))
-        {
-            equipped.UseItem(this);
-        }
-
-        //interact item - E
-        GameObject closestObject = interaction.closest;
-        if (Input.GetKeyDown("e"))
-        {
-            if (closestObject == null) // nothing to interact with
+            //spawn item - K (axe)
+            if (Input.GetKeyDown("k"))
             {
-                Debug.Log("There is no item in interacting distance");
+                GameObject spawnContainer = Instantiate(itemContainerPrefab, (this.transform.position + this.transform.forward * 1.01f + new Vector3(0f, 0.5f, 0f)), Random.rotation);
+                Axe newItem = spawnContainer.AddComponent(typeof(Axe)) as Axe;
+                Debug.Log("Spawned a " + newItem.Name + " " + newItem.Title + ".");
 
+                spawnContainer.GetComponent<Rigidbody>().AddForce(this.transform.forward * throwForce);
+                spawnContainer.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f)));
             }
-            else // something to interact with!
-            {
-                Item pickup = closestObject.GetComponent<Item>();
-                if (pickup != null) // interactable is an item
-                {
 
-                    if (equipped == null) // pick up the item
+            if (Input.GetKeyDown("m"))
+            {
+                Debug.Log(Screen.currentResolution);
+                Debug.Log(Screen.height);
+                Debug.Log(Screen.width);
+            }
+
+            //drop item - X
+            if (equipped != null && Input.GetKeyDown("x"))
+            {
+                GameObject spawnContainer = Instantiate(itemContainerPrefab, (this.transform.position + this.transform.forward * 1.01f + new Vector3(0f, 0.5f, 0f)), Random.rotation);
+                Item newItem = spawnContainer.AddComponent(equipped.GetType()) as Item;
+                Debug.Log("Dropped a " + equipped.Name + ".");
+                inventoryPic001.gameObject.SetActive(false);
+                equipped = null;
+
+                spawnContainer.GetComponent<Rigidbody>().AddForce(this.transform.forward * throwForce);
+                spawnContainer.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f)));
+            }
+
+            //use item - MOUSE1
+            if (equipped != null && Input.GetMouseButtonDown(0))
+            {
+                equipped.UseItem(this);
+            }
+
+            //interact item - E
+            GameObject closestObject = interaction.closest;
+            if (Input.GetKeyDown("e"))
+            {
+                if (closestObject == null) // nothing to interact with
+                {
+                    Debug.Log("There is no item in interacting distance");
+
+                }
+                else // something to interact with!
+                {
+                    Item pickup = closestObject.GetComponent<Item>();
+                    if (pickup != null) // interactable is an item
                     {
-                        equipped = pickup;
-                        Debug.Log("Picked up '" + pickup.Name + "'!");
-                        
-                        // For now just pick up Axe
-                        inventoryPic001.sprite = axeSprite;
-                        inventoryPic001.gameObject.SetActive(true);
-                        //closestObject
+
+                        if (equipped == null) // pick up the item
+                        {
+                            equipped = pickup;
+                            Debug.Log("Picked up '" + pickup.Name + "'!");
+                            
+                            // For now just pick up Axe
+                            inventoryPic001.sprite = axeSprite;
+                            inventoryPic001.gameObject.SetActive(true);
+                            //closestObject
+                            //interaction.DestroyObject(closestObject);
+                            //Destroy(closestObject);
+                        }
+                        else // already have an item - do nothing
+                        {
+                            Debug.Log("Cannot pick up '" + pickup.Name + "' - you already have an item.");
+                            closestObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 4f), Random.Range(0f, 1f)));
+                            closestObject.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f)));
+                        }
+
+                    }
+                    else //interactable is not an item
+                    {
+                        Debug.Log("Interacted with '" + closestObject.GetComponent<Item>().Name + "' !");
                         //interaction.DestroyObject(closestObject);
-                        //Destroy(closestObject);
                     }
-                    else // already have an item - do nothing
-                    {
-                        Debug.Log("Cannot pick up '" + pickup.Name + "' - you already have an item.");
-                        closestObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 4f), Random.Range(0f, 1f)));
-                        closestObject.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(0f, 2f), Random.Range(0f, 2f), Random.Range(0f, 2f)));
-                    }
-
-                }
-                else //interactable is not an item
-                {
-                    Debug.Log("Interacted with '" + closestObject.GetComponent<Item>().Name + "' !");
-                    //interaction.DestroyObject(closestObject);
                 }
             }
-        }
-        //update closest interactable label
-        if (closestObject != null)
-        {
-            interactLabel.transform.position = closestObject.transform.position + new Vector3(0f, 0.4f, 0f);
-            interactLabel.transform.rotation = Camera.main.transform.rotation;
-            interactLabel.GetComponent<TextMesh>().text = closestObject.GetComponent<Item>().Name;
-
-        }
-        else
-        {
-            interactLabel.GetComponent<TextMesh>().text = "";
-        }
-
-        // Inventory scroll wheel functionality
-        var delta = Input.GetAxis("Mouse ScrollWheel");
-        if (delta > 0f)
-        {
-            // Scrolling down goes left in inventory
-            if (currInventoryIndex == 0)
+            //update closest interactable label
+            if (closestObject != null)
             {
-                currInventoryIndex = 5;
-            }
+                interactLabel.transform.position = closestObject.transform.position + new Vector3(0f, 0.4f, 0f);
+                interactLabel.transform.rotation = Camera.main.transform.rotation;
+                interactLabel.GetComponent<TextMesh>().text = closestObject.GetComponent<Item>().Name;
 
+            }
             else
             {
-                currInventoryIndex = (currInventoryIndex - 1) % 5;
+                interactLabel.GetComponent<TextMesh>().text = "";
             }
-        }
-        else if (delta < 0f)
-        {
-            // Scrolling up goes right in inventory
-            currInventoryIndex = (currInventoryIndex + 1) % 5;
-        }
 
-        // Highlight selected inventory item
-        var tempColor001 = inventoryItem001.color;
-        var tempColor002 = inventoryItem002.color;
-        var tempColor003 = inventoryItem003.color;
-        var tempColor004 = inventoryItem004.color;
-        var tempColor005 = inventoryItem005.color;
-        if (currInventoryIndex == 0)
-        {
-            tempColor001.a = 1f;
-            tempColor002.a = 0.2f;
-            tempColor003.a = 0.2f;
-            tempColor004.a = 0.2f;
-            tempColor005.a = 0.2f;
-        }
+            // Inventory scroll wheel functionality
+            var delta = Input.GetAxis("Mouse ScrollWheel");
+            if (delta > 0f)
+            {
+                // Scrolling down goes left in inventory
+                if (currInventoryIndex == 0)
+                {
+                    currInventoryIndex = 5;
+                }
 
-        else if (currInventoryIndex == 1)
-        {
-            tempColor001.a = 0.2f;
-            tempColor002.a = 1f;
-            tempColor003.a = 0.2f;
-            tempColor004.a = 0.2f;
-            tempColor005.a = 0.2f;
-        }
+                else
+                {
+                    currInventoryIndex = (currInventoryIndex - 1) % 5;
+                }
+            }
+            else if (delta < 0f)
+            {
+                // Scrolling up goes right in inventory
+                currInventoryIndex = (currInventoryIndex + 1) % 5;
+            }
 
-        else if (currInventoryIndex == 2)
-        {
-            tempColor001.a = 0.2f;
-            tempColor002.a = 0.2f;
-            tempColor003.a = 1f;
-            tempColor004.a = 0.2f;
-            tempColor005.a = 0.2f;
-        }
+            // Highlight selected inventory item
+            var tempColor001 = inventoryItem001.color;
+            var tempColor002 = inventoryItem002.color;
+            var tempColor003 = inventoryItem003.color;
+            var tempColor004 = inventoryItem004.color;
+            var tempColor005 = inventoryItem005.color;
+            if (currInventoryIndex == 0)
+            {
+                tempColor001.a = 1f;
+                tempColor002.a = 0.2f;
+                tempColor003.a = 0.2f;
+                tempColor004.a = 0.2f;
+                tempColor005.a = 0.2f;
+            }
 
-        else if (currInventoryIndex == 3)
-        {
-            tempColor001.a = 0.2f;
-            tempColor002.a = 0.2f;
-            tempColor003.a = 0.2f;
-            tempColor004.a = 1f;
-            tempColor005.a = 0.2f;
-        }
+            else if (currInventoryIndex == 1)
+            {
+                tempColor001.a = 0.2f;
+                tempColor002.a = 1f;
+                tempColor003.a = 0.2f;
+                tempColor004.a = 0.2f;
+                tempColor005.a = 0.2f;
+            }
 
-        else 
-        {
-            tempColor001.a = 0.2f;
-            tempColor002.a = 0.2f;
-            tempColor003.a = 0.2f;
-            tempColor004.a = 0.2f;
-            tempColor005.a = 1f;
+            else if (currInventoryIndex == 2)
+            {
+                tempColor001.a = 0.2f;
+                tempColor002.a = 0.2f;
+                tempColor003.a = 1f;
+                tempColor004.a = 0.2f;
+                tempColor005.a = 0.2f;
+            }
+
+            else if (currInventoryIndex == 3)
+            {
+                tempColor001.a = 0.2f;
+                tempColor002.a = 0.2f;
+                tempColor003.a = 0.2f;
+                tempColor004.a = 1f;
+                tempColor005.a = 0.2f;
+            }
+
+            else 
+            {
+                tempColor001.a = 0.2f;
+                tempColor002.a = 0.2f;
+                tempColor003.a = 0.2f;
+                tempColor004.a = 0.2f;
+                tempColor005.a = 1f;
+            }
+            // Assign new colors
+            inventoryItem001.color = tempColor001;
+            inventoryItem002.color = tempColor002;
+            inventoryItem003.color = tempColor003;
+            inventoryItem004.color = tempColor004;
+            inventoryItem005.color = tempColor005;
         }
-        // Assign new colors
-        inventoryItem001.color = tempColor001;
-        inventoryItem002.color = tempColor002;
-        inventoryItem003.color = tempColor003;
-        inventoryItem004.color = tempColor004;
-        inventoryItem005.color = tempColor005;
     }
 
     private void FixedUpdate()
