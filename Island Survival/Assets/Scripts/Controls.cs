b4b26333d87d5e43583125c5dffdcd66a65739e8
Michael Williams <mjwilli8@umd.edu>
2019-05-19 12:57:41 -0400

    added code for getting all players to controls script

--- Island Survival/Assets/Scripts/Controls.cs
+++ Island Survival/Assets/Scripts/Controls.cs
@@ -17,6 +17,9 @@ public class Controls : MonoBehaviour
     public GameObject screen;
     public Image pauseMenu;
 
+    // Array to store all players
+    public GameObject[] players;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +28,9 @@ public class Controls : MonoBehaviour
 
     void Update()
     {
+        // Grab all players
+        players = GameObject.FindGameObjectsWithTag("Player");
+        
     	// Restarts the game
     	if (Input.GetKeyDown("r") )
     	{
